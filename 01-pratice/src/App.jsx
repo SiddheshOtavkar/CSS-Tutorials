4f9aa297cf882376aac2a5a20207561e17479249
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-03-07 20:01:00 +0530

    Commit

--- 01-pratice/src/App.jsx
+++ 01-pratice/src/App.jsx
@@ -1,11 +1,11 @@
-// import './App.css'
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
